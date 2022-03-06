8c266d11962294617b9a3c64d3114d5d3f30aeab
ShehbazWaasi20 <shehbazwaasi@gmail.com>
2022-03-06 21:45:39 +0530

    assignment 5

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+   res=[]
+   for (i=0;i<arr.length;i++){
+       if(arr[i]%2===0){
+           res.push(arr[i])
+       }
+   }
+   return res
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+   res=[]
+   for(let i=0;i<arr.length;i++){
+       let mul=arr[i]*n
+       res.push(mul)
+   }
+   return res
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +40,12 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+   for(i=0;i<arr.length;i++){
+       if(i==n){
+           arr.splice(i,1)
+       }
+   }
+   return arr
 }
 
 module.exports = {
