943f049eff2454251fef3ae36d500f8ed945c3a3
ShehbazWaasi20 <shehbazwaasi@gmail.com>
2022-03-14 12:05:27 +0530

    assignment7

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,23 +1,22 @@
-count=0
-document.getElementById("addTodo").onclick = function(){
-    let input =document.querySelector("#todoinput").value;
-    //alert("Process executing")
-    if (input.length===0){
-        alert("please enter something")
-    }else{
-        document.querySelector(".todos").innerHTML += `
-        <p key=${count}>
-        <span class="task">
-        ${input}
-        </span>
-        </p>
-        `;
-        count++;
-        let currentTask = document.querySelectorAll('.task');
-        for(let i=0; i<currentTask.length; i++){
-            currentTask[i].onclick=function(){
-                this.parentNode.remove()
-            }
+const newTodoInput = document.querySelector('input');
+const addTodoButtom = document.querySelector('button');
+const todolist = document.querySelector('div.todos');
+let count = 0;
+addTodoButtom.onclick = function(){
+    let input = newTodoInput.value;
+    if (input.length === 0){
+        alert("Please Enter a Task");
+    }
+    else{
+        let p = document.createElement('p')
+        p.setAttribute("key", count)
+        p.innerHTML = input
+        todolist.appendChild(p)
+        newTodoInput.value=""
+        document.querySelector(`p[key = "${count}"]`).addEventListener("click",myfunction);
+        function myfunction(){
+            todolist.removeChild(this)
         }
+        count++
     }
 }
